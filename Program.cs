782bc8e666e62c150349a7bc1d6888666134c47a
magali <magalipuuujol@gmail.com>
2021-12-14 10:05:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace problema_clientes_FD_szicom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
